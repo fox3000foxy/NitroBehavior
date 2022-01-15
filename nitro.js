6df25398cc2b203e3ab1e2029b9d4e1cf7a3eab2
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2022-01-15 14:38:07 +0100

    Add files via upload

--- nitro.js
+++ nitro.js
@@ -13,10 +13,10 @@ function inject_stickers(packs) {
 }
 
 function changeArrow(detailsElem) {
-    if (detailsElem.hasAttribute('open'))
-        detailsElem.querySelector('.packArrow').innerHTML = '<svg class="arrow-gKvcEx headerCollapseIcon-ymwSPR headerCollapseIconCollapsed-3fap1D" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg>'
+    if (!detailsElem.hasAttribute('open'))
+        detailsElem.querySelector('.packArrow').innerHTML = '<svg class="arrow-2HswgU headerCollapseIcon-3WeMjJ" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg>'
     else
-        detailsElem.querySelector('.packArrow').innerHTML = '<svg class="arrow-gKvcEx headerCollapseIcon-ymwSPR" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg>'
+        detailsElem.querySelector('.packArrow').innerHTML = '<svg class="arrow-2HswgU headerCollapseIcon-3WeMjJ headerCollapseIconCollapsed-3C20LE" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg>'
 }
 
 function inject_sticker_pack(pack, lines) {
@@ -29,17 +29,19 @@ function inject_sticker_pack(pack, lines) {
     pack_elem.innerHTML = `
     <details>
 		<summary style="list-style: none;">
-			<div class="wrapper-1-Fsb8 packHeader-NLJ7S5">
-				<div class="header-19cWci interactive-BzuinF" aria-expanded="true" aria-label="Pack ${pack_name}" role="button" tabindex="0">
-					<div aria-hidden="true" class="headerIcon-1qrXTN">
+			<div class="wrapper-1NNaWG packHeader-w9jpYl">
+				<div class="header-1XpmZs interactive-MpGq2z" aria-expanded="true" aria-label="Pack ${pack_name}" role="button" tabindex="0">
+					<div aria-hidden="true" class="headerIcon-2Gk2OH">
 						<div>
 							<svg width="16" height="16" class="mask-2hO5M8" viewBox="0 0 16 16">
-								<foreignObject x="0" y="0" width="16" height="16" overflow="visible" mask="url(#svg-mask-squircle)"><img alt="${pack_name}" src="https://aventuros.fr/discord-stickers/thumbnails/${cover_sticker['id']}.png" class="guildIcon-3h-1IH"></foreignObject>
+								<foreignObject x="0" y="0" width="16" height="16" overflow="visible" mask="url(#svg-mask-squircle)"><img alt="${pack_name}" src="https://aventuros.fr/discord-stickers/thumbnails/${cover_sticker['id']}.png" class="guildIcon-2SUGiq"></foreignObject>
 							</svg>
 						</div>
 					</div>
-				<span class="headerLabel-3dG4M-">${pack_name}</span>
-				<div class="packArrow"><svg class="arrow-gKvcEx headerCollapseIcon-ymwSPR headerCollapseIconCollapsed-3fap1D" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg></div>
+				<span class="headerLabel-1g790w">${pack_name}</span>
+				<div class="packArrow">
+				<svg class="arrow-2HswgU headerCollapseIcon-3WeMjJ headerCollapseIconCollapsed-3C20LE" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.59 8.59004L12 13.17L7.41 8.59004L6 10L12 16L18 10L16.59 8.59004Z"></path></svg>
+				</div>
 				</div>
 			</div>
 		</summary>
@@ -49,10 +51,11 @@ function inject_sticker_pack(pack, lines) {
     for (const [id, sticker] of Object.entries(pack['stickers'])) {
         if (lines % 1 == 0) {
             sticker_line = document.createElement("div");
-            sticker_line.setAttribute("class", "row-2psonc");
+            sticker_line.setAttribute("class", "row-2mBMW2");
             sticker_line.setAttribute("role", "row");
             sticker_line.setAttribute("aria-rowindex", "1");
             sticker_line.setAttribute("style", "column-gap: 8px; grid-template-columns: repeat(auto-fill, 84px); height: 84px;");
+			// console.log(pack_elem)
 
         }
         sticker['id'] = id;
@@ -70,7 +73,7 @@ function inject_sticker_pack(pack, lines) {
 
 function addSticker(pack_name, pack_id, sticker) {
     let sticker_elem = document.createElement("div");
-    sticker_elem.setAttribute("class", "sticker-2cpdZp sticker-3HpuRA stickerInspected-2EM4w-");
+    sticker_elem.setAttribute("class", "sticker-H2HhJD sticker-2Yb_-L");
     sticker_elem.setAttribute("role", "gridcell");
     sticker_elem.setAttribute("aria-rowindex", "1");
     sticker_elem.setAttribute("aria-colindex", "1");
@@ -80,9 +83,9 @@ function addSticker(pack_name, pack_id, sticker) {
     sticker_elem.setAttribute("data-type", "sticker");
     sticker_elem.setAttribute("data-id", sticker['id']);
     sticker_elem.setAttribute("style", "width: 80px; height: 80px; padding: 2px;");
-    sticker_elem.innerHTML = `<div class="stickerNode-20m2uy stickerUnsendable-2q_h2B">
-    <div class="assetWrapper-3GNt0z assetWrapperMasked-2tj_Bb" style="height: 80px; width: 80px;">
-        <img class="pngImage-33yLRP stickerAsset-13j1W0" alt="${sticker['description']}" src="https://aventuros.fr/discord-stickers/${pack_id}/${sticker['id']}.${sticker['format']}" draggable="false" data-type="sticker" data-id="${sticker['id']}">
+    sticker_elem.innerHTML = `<div class="stickerNode-3aUBs4 stickerUnsendable-2q_h2B">
+    <div class="assetWrapper-2hzITV" style="height: 80px; width: 80px;">
+        <img class="pngImage-1vIgLy stickerAsset-4c7Oqy" alt="${sticker['description']}" src="https://aventuros.fr/discord-stickers/${pack_id}/${sticker['id']}.${sticker['format']}" draggable="false" data-type="sticker" data-id="${sticker['id']}">
     </div>`;
 
     return sticker_elem;
@@ -93,8 +96,8 @@ function sendEmoji(emojiName, emojiUrl, emojiBool) {
     reqJSON = {
         emojiName,
         emojiUrl: escape(emojiUrl.split("size=")[0] + (emojiBool ? "size=48" : (emojiUrl.indexOf("aventuros.fr") == -1 ? "size=160" : ""))),
-        name: document.querySelector("#app-mount > div > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.sidebar-2K8pFh > section > div.container-3baos1 > div.nameTag-3uD-yy.canCopy-2VBT7N > div.colorStandard-2KCXvj.size14-e6ZScH.usernameContainer-1fp4nu > div").innerHTML,
-        avatarUrl: escape(document.querySelector("#app-mount > div > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.sidebar-2K8pFh > section > div.container-3baos1 > div.avatarWrapper-2yR4wp > div > svg > foreignObject > div > img").src),
+        name: document.querySelector("#app-mount > div > div > div > div > div > div > div > div > section > div > div > div > div").innerHTML,
+        avatarUrl: escape(document.querySelector("#app-mount > div > div > div > div > div > div > div > div > section > div > div > div > svg > foreignObject > div > img").src),
         serverId: location.href.split("/")[4],
         channelId: location.href.split("/")[5]
     }
@@ -142,13 +145,16 @@ function main() {
     //Boucle principale
     setInterval(function() {
         //Emojis
-        let listDisabled = document.querySelectorAll(".emojiItemDisabled-1FvFuF");
+        let listDisabled = document.querySelectorAll("[class*=emojiItemDisabled]");
         listDisabled.forEach((emojiElem) => {
             emojiElem.addEventListener("mousedown", () => {
                 sendEmoji(emojiElem.getAttribute('data-name'), emojiElem.firstChild.src, true);
-                document.querySelector("div.buttons-3JBrkn > div:nth-child(4) > button").click();
+                document.querySelector("[class*=buttons] > div:nth-child(4) > button").click();
             })
-            emojiElem.classList.remove("emojiItemDisabled-1FvFuF");
+            // emojiElem.classList.remove("emojiItemDisabled-1FvFuF");
+			const prefix = "emojiItemDisabled";
+			const classes = emojiElem.className.split(" ").filter(c => !c.startsWith(prefix));
+			emojiElem.className = classes.join(" ").trim();
         })
         resetCounter = 0;
 
@@ -158,22 +164,28 @@ function main() {
                 document.getElementById("sticker-picker-grid").setAttribute("nitrochecked", '');
                 setTimeout(() => {
                     let lines = inject_stickers(window.stickerIndex);
-					let oldHeight = document.getElementsByClassName("listHeight-3jP-vC")[0].style.height;
-                    document.getElementsByClassName("listHeight-3jP-vC")[0].style.height = `${parseInt(oldHeight.substring(0, oldHeight.length -2))+lines/12}px`;                   
+					let oldHeight = document.querySelectorAll("[class*=listHeight]")[0].style.height;
+                    document.querySelectorAll("[class*=listHeight]")[0].style.height = `${parseInt(oldHeight.substring(0, oldHeight.length -2))+lines/12}px`;                   
                 }, 1000);
             }
-            listDisabled = document.querySelectorAll(".stickerUnsendable-2q_h2B");
+            listDisabled = document.querySelectorAll("[class*=stickerUnsendable]");
             listDisabled.forEach((stickerElem) => {
                 stickerElem.addEventListener("mousedown", () => {
                     sendEmoji(stickerElem.firstElementChild.firstElementChild.alt, stickerElem.firstElementChild.firstElementChild.src, false);
-                    document.querySelector("div.buttons-3JBrkn > div:nth-child(3) > button").click();
+                    document.querySelector("[class*=buttons] > div:nth-child(3) > button").click();
                 })
                 console.log()
                 if (stickerElem.firstElementChild.children[1] != undefined) {
+					// const prefix = "prefix";
+					// const classes = el.className.split(" ").filter(c => !c.startsWith(prefix));
+					// el.className = classes.join(" ").trim();
                     let classList = [...stickerElem.firstElementChild.children[1].classList]
-                    if (classList.indexOf("loadingIndicator-1T4i1D") != -1) stickerElem.firstElementChild.children[1].classList.remove("loadingIndicator-1T4i1D")
+                    // if (classList.indexOf("loadingIndicator-1T4i1D") != -1) stickerElem.firstElementChild.children[1].classList.remove("loadingIndicator-1T4i1D")
                 }
-                stickerElem.classList.remove("stickerUnsendable-2q_h2B");
+				const prefix = "stickerUnsendable";
+				const classes = stickerElem.className.split(" ").filter(c => !c.startsWith(prefix));
+				stickerElem.className = classes.join(" ").trim();
+                // stickerElem.classList.remove("stickerUnsendable-PkQAxI");
             });
         }
     }, 200);
@@ -183,12 +195,12 @@ function main() {
 Object.defineProperty(window, 'localStorage', getLocalStoragePropertyDescriptor());
 window.localStorage = getLocalStoragePropertyDescriptor().get.call(window);
 var wait = setInterval(function() {
-    if (document.querySelector("#app-mount > div > div > div.layers-3iHuyZ.layers-3q14ss > div > div > nav > ul > div.scroller-1Bvpku.none-2Eo-qx.scrollerBase-289Jih > div.tutorialContainer-11ICd5 > div > div.listItemWrapper-2MsAsM > div > svg > foreignObject")) {
+    if (document.querySelector("#app-mount > div > div > div > div > div > nav > ul > div > div > div > div > div > svg > foreignObject")) {
         if (document.body.hasAttribute("nitrostarted")) {clearInterval(wait);} else {document.body.setAttribute("nitrostarted", '');setTimeout(main, 0);}
     }
 }, 200);
 
-if (Math.random() < 0.1) {
-    var tipTextElem = document.querySelector("div > div.content-1-zrf2 > div > div.tip-2cgoli");
-    if (tipTextElem) {tipTextElem.innerText = "La couleur du badge nitro par l'extension peut être changée en cliquant sur votre badge";}
-}
+// if (Math.random() < 0.1) {
+    // var tipTextElem = document.querySelector("div > div > div > div");
+    // if (tipTextElem) {tipTextElem.innerText = "La couleur du badge nitro par l'extension peut être changée en cliquant sur votre badge";}
+// }
